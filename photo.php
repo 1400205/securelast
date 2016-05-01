3bee0424ab676317df4ac52312d9a656b4ae970a
1400205 <1400205@rgu.ac.uk>
2016-05-01 05:39:14 +0100

    output filtering

--- photo.php
+++ photo.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
 
 //Function to cleanup user input for xss
 function xss_cleaner($input_str) {
-    $return_str = str_replace( array('<','>',"'",'"',')','(','/'), array('&lt;','&gt;','&apos;','&#x22;','&#x29;','&#x28;','&mm'), $input_str );
+    $return_str = str_replace( array('<','>',"'",'"',')','('), array('&lt;','&gt;','&apos;','&#x22;','&#x29;','&#x28;'), $input_str );
     $return_str = str_ireplace( '%3Cscript', '', $return_str );
     return $return_str;
 }
