f0c36a0ea9f04369979ffa99430b6188c39f2c1c
1400205 <1400205@rgu.ac.uk>
2016-05-01 03:00:39 +0100

    implement prepared statement on this page

--- login.php
+++ login.php
@@ -1,11 +1,8 @@
+
 <?php
-
-//display error
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
+?>
+<?php
 include("connection.php"); //Establishing connection with our database
 
 $error = ""; //Variable for storing our errors.
@@ -17,22 +14,24 @@ if(isset($_POST["submit"]))
     }else
     {
         // Define $username and $password
-        $username1=$_POST['username'];
+        $username=$_POST['username'];
         $password=$_POST['password'];
 
-
-        // To protect from MySQL injection and XSS
-        $username1 = stripslashes($username1);
-        $password = stripslashes($password);
-        $username1 = mysqli_real_escape_string($db, $username1);
-        $password = mysqli_real_escape_string($db, $password);
-        $username1 = htmlspecialchars($username1);
-        $password = htmlspecialchars($password);
-
-        $password = md5($password);
+        //clean input photo user name
+        $username = stripslashes( $username );
+        $username=mysqli_real_escape_string($db,$username);
+        $username = htmlspecialchars($username);
+        $password=md5($password);
 
 
 
+        //implement prepared statement to take of sql injection and other vulnerabilities
+
+        //declare instance of connection
+        $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+        if (!($sqlcon->connect_errno)){
+            echo"connection Failed";
+        }
 
         //prepare statement
         if($stmt=$sqlcon->prepare("SELECT userID FROM usersSecure WHERE username=? and password=?")){
@@ -45,18 +44,19 @@ if(isset($_POST["submit"]))
 
 
         if( ($row=$result->fetch_row()))
-
-
-        // Initializing Session
         {
             $_SESSION['username'] = $username; // Initializing Session
             $_SESSION["userid"] = $row[0];//user id assigned to session global variable
             $_SESSION["timeout"] = time();//get session time
             $_SESSION["ip"] = $_SERVER['REMOTE_ADDR'];//get session time
-            header("location: photos.php"); // Redirecting To Other Page
-        }
 
+            header("location: photos.php"); // Redirecting To Other Page
+        }else
+        {
+            $error = "Incorrect username or password.";
+        }
 
     }
 }
+
 ?>
