3a7538c3001d02895bcccee9c16ebddd7d10a871
1400205 <1400205@rgu.ac.uk>
2016-05-01 02:43:27 +0100

    implement prepared statement on this page

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
+
 <?php
 session_start();
 ?>
 <?php
-//include("connection.php"); //Establishing connection with our database
-//declare instance of connection
-$sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+include("connection.php"); //Establishing connection with our database
 
 $error = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
@@ -14,20 +13,22 @@ if(isset($_POST["submit"]))
         $error = "Both fields are required.";
     }else
     {
-
-
         // Define $username and $password
         $username=$_POST['username'];
         $password=$_POST['password'];
+
         //clean input photo user name
         $username = stripslashes( $username );
         $username=mysqli_real_escape_string($db,$username);
         $username = htmlspecialchars($username);
         $password=md5($password);
 
+
+
         //implement prepared statement to take of sql injection and other vulnerabilities
 
-
+        //declare instance of connection
+        $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
         if (!($sqlcon->connect_errno)){
             echo"connection Failed";
         }
@@ -41,8 +42,6 @@ if(isset($_POST["submit"]))
             $result = $stmt->get_result();
         }
 
-        //If username and password exist in our database then create a session.
-        //Otherwise echo error.
 
         if( ($row=$result->fetch_row()))
         {
