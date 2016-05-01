8eb3ea3f5762a3a01ccf3f72b77a806fc2d2824f
1400205 <1400205@rgu.ac.uk>
2016-05-01 02:19:45 +0100

    check login functionality with prepared statements

--- login.php
+++ login.php
@@ -13,20 +13,40 @@ if(isset($_POST["submit"]))
         // Define $username and $password
         $username=$_POST['username'];
         $password=$_POST['password'];
+        //clean input photo user name
+        $username = stripslashes( $username );
+        $username=mysqli_real_escape_string($db,$username);
+        $username = htmlspecialchars($username);
+        $password=md5($password);
 
+        //implement prepared statement to take of sql injection and other vulnerabilities
 
+        //declare instance of connection
+        $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-        //Check username and password from database
-        $sql="SELECT userID FROM usersSecure WHERE username='$username' and password='$password'";
-        $result=mysqli_query($db,$sql);
-        $row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+        if (!($sqlcon->connect_errno)){
+            echo"connection Failed";
+        }
+
+        //prepare statement
+        if($stmt=$sqlcon->prepare("SELECT userID FROM usersSecure WHERE username=? and password=?")){
+            //bind parameter
+            $stmt->bind_param('ss',$username,$password);
+            $stmt->execute();
+            //get result
+            $result = $stmt->get_result();
+        }
 
         //If username and password exist in our database then create a session.
         //Otherwise echo error.
 
-        if(mysqli_num_rows($result) == 1)
+        if( ($row=$result->fetch_row()))
         {
             $_SESSION['username'] = $username; // Initializing Session
+            $_SESSION["userid"] = $row[0];//user id assigned to session global variable
+            $_SESSION["timeout"] = time();//get session time
+            $_SESSION["ip"] = $_SERVER['REMOTE_ADDR'];//get session time
+
             header("location: photos.php"); // Redirecting To Other Page
         }else
         {
