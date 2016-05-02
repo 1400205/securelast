778ad368f6b134d60139d58804b70888462ec35e
1400205 <1400205@rgu.ac.uk>
2016-05-02 07:45:12 +0100

    output filtering

--- photos.php
+++ photos.php
@@ -10,18 +10,7 @@ $ip=$_SESSION["ip"];
 $timeout=$_SESSION ["timeout"];
 
 
-if (!($ip==$_SERVER['REMOTE_ADDR'])){
-	header("location: logout.php"); // Redirecting To Other Page
-}
 
-if($_SESSION ["timeout"]+60 < time()){
-
-	//session timed out
-	header("location: logout.php"); // Redirecting To Other Page
-}else{
-	//reset session time
-	$_SESSION['timeout']=time();
-}
 ?>
 
 <!doctype html>
