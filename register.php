d5b6e374f5b71c4e45d4b3d9589a7607208568a5
1400205 <1400205@rgu.ac.uk>
2016-05-02 08:21:00 +0100

    output filtering

--- register.php
+++ register.php
@@ -2,22 +2,7 @@
 
 	include("connection.php");
 	include("submit.php");
-$ip=$_SESSION["ip"];
-$timeout=$_SESSION ["timeout"];
 
-
-if (!($ip==$_SERVER['REMOTE_ADDR'])){
-	header("location: logout.php"); // Redirecting To Other Page
-}
-
-if($_SESSION ["timeout"]+60 < time()){
-
-	//session timed out
-	header("location: logout.php"); // Redirecting To Other Page
-}else{
-	//reset session time
-	$_SESSION['timeout']=time();
-}
 
 ?>
 <!doctype html>
